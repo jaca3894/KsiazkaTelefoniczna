833c6a6f5d1a1895650045fedcfba6f8b3b5ed60
jaca3894 <157138800+jaca3894@users.noreply.github.com>
2024-01-19 17:21:46 +0100

    Update KsiazkaTelefoniczna.java

--- src/KsiazkaTelefoniczna.java
+++ src/KsiazkaTelefoniczna.java
@@ -18,7 +18,6 @@ public class KsiazkaTelefoniczna extends Metody
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-
     public static final String ANSI_CYAN = "\u001B[36m";
 
 
